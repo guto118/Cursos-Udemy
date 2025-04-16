b3cb0690488768195f022e6881ffb2a635d83e61
Gustavo Pedro <106572508+guto118@users.noreply.github.com>
2025-04-16 11:37:16 -0300

    commit todos cursos

---
+++ Curso_FullStack/seccao JavaScript/aprendizado/java-script/meu-script.js
@@ -0,0 +1,22 @@
+
+alert('Olá, seja bem vindo ao curso!!!')
+
+document.getElementById('nome').value = 'Oi'
+
+//comentário de uma linha
+
+/*
+Comentário de multiplas linhas 
+Essetipo de comentário permite que várias linhas sejam encapsuladas em um mesmo comentário
+*/
+
+
+
+/*
+Tipos de variáveis 
+
+string
+number <- int / float
+boolean
+
+*/
