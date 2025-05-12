676937e34b204c253daeab58a9f0d57a3fc28766
Gustavo Pedro <106572508+guto118@users.noreply.github.com>
2025-05-12 15:49:35 -0300

    12/05/2025
    estudos de hoje

--- Curso_FullStack/seccao JavaScript/aprendizado/javaScript/meu-script.js
+++ Curso_FullStack/seccao JavaScript/aprendizado/javaScript/meu-script.js
@@ -1,22 +1,25 @@
 
 alert('Olá, seja bem vindo ao curso!!!')
 
+
+
 document.getElementById('nome').value = 'Oi'
 
 //comentário de uma linha
 
 /*
 Comentário de multiplas linhas 
-Essetipo de comentário permite que várias linhas sejam encapsuladas em um mesmo comentário
+Esse tipo de comentário permite que várias linhas sejam encapsuladas em um mesmo comentário
 */
 
 
 
-/*
+/*no javaScript não precisam ser definidas anteriormente
+as variáveis 
 Tipos de variáveis 
 
 string
 number <- int / float
-boolean
+boolean <- true / false
 
 */
