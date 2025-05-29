f79f771446ca0e2de3b196e3b7e04f37cdc0929d
guto118 <gu.pedro1212@gmail.com>
2025-05-29 13:16:18 -0300

    29/0502025
    Inicio do curso de react

---
+++ Curso_React/1_INTRO/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Word React!</h1>
+      <h2>Emmet funcionou</h2>
+    </div>
+  );
+}
+
+export default App;
