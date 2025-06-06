554ec6fcfee26208cff76f5369379ff06a3bd26b
guto118 <gu.pedro1212@gmail.com>
2025-06-06 00:24:53 -0300

    05/06/2025

---
+++ Curso_FullStack/seccao JavaScript/aprendizado2/javaScript/array_basico.js
@@ -0,0 +1,43 @@
+//todos são formas de declarar arrays no javaScript
+
+// var lista_frutas = Array();
+/*
+lista_frutas[1] = 'Banana' ;
+lista_frutas[2] = 'Maçã' ;
+lista_frutas[3] = 'Morango' ;
+lista_frutas[4] = 'Uva' ;
+*/
+/*
+var lista_frutas = [];
+
+lista_frutas[1] = 'Banana' ;
+lista_frutas[2] = 'Maçã' ;
+lista_frutas['x'] = 'Morango' ;
+lista_frutas[4] = 'Uva' ;
+
+*/
+
+
+// essas próximas não é possível escolher o índice
+//eles são colocados padronizadamente e começam por 0
+
+
+// var lista_frutas = Array('Banana', 'Maçã', 'Morango', 'Uva');
+
+//var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'];
+
+
+
+/*
+ O atributo lenght está disponível para os os array(S) e para as strings 
+ o length vai funcionar no array somente se os elementos forem anexados forem indexados por valores numéricos sequenciais sem intervalo e que iniciem de 0
+*/
+var lista_frutas = Array();
+
+lista_frutas[1] = 'Banana' ;
+lista_frutas[2] = 'Maçã' ;
+lista_frutas['x'] = 'Morango' ;
+lista_frutas[4] = 'Uva' ;
+
+
+console.log(lista_frutas);
---
+++ Curso_FullStack/seccao JavaScript/aprendizado2/javaScript/array_multidimensional.js
@@ -0,0 +1,15 @@
+var lista_coisas = Array();
+
+lista_coisas['frutas'] = Array('Banana', 'Maçã', 'Morango', 'Uva');
+
+lista_coisas ['pessoas'] = ['João', 'José', 'Maria'];
+
+/*
+lista_coisas['frutas'][1] = 'Banana';
+lista_coisas['frutas'][2] = 'Maçã';
+lista_coisas['frutas'][3] = 'Morango';
+lista_coisas['frutas'][4] = 'Uva';
+*/
+console.log(lista_coisas);
+
+document.writeln(lista_coisas['frutas'][3]);
