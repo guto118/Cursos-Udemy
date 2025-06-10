180a88c383468473ed62fc46890e27465a000d28
guto118 <gu.pedro1212@gmail.com>
2025-06-10 01:33:49 -0300

    09/06/2025
    atualização freecodcamp

---
+++ Curso_FullStack/seccao JavaScript/aprendizado2/javaScript/incluir_remover.js
@@ -0,0 +1,22 @@
+var lista_coisas = Array();
+
+lista_coisas['frutas'] = Array();
+lista_coisas['pessoas'] = [];
+
+
+
+//incluir um elementono final do array
+lista_coisas['frutas'].push('Banana');
+lista_coisas['frutas'].push('Uva');
+
+//incluir elemento no início do array
+//lista_frutas.unshift('Uva');
+
+//excluir um elemento do final do array
+//lista_frutas.pop();
+
+//excluir um elemento do início do array
+//lista_frutas.shift();
+
+
+console.log(lista_coisas);
